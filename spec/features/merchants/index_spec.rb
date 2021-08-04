5ce777537b8a9d47f629cc9c335f9c2c34d6ddc2
Daniel <danmoran.pro@gmail.com>
2021-08-04 10:43:59 -0600

    faker and simple cov implemented

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 RSpec.describe 'merchants index page', type: :feature do
   describe 'As a user' do
     before :each do
-      @bike_shop = Merchant.create(name: "Brian's Bike Shop")
-      @dog_shop = Merchant.create(name: "Meg's Dog Shop")
+      @merchant_1 = create(:random_merchant)
+      @merchant_2 = create(:random_merchant)
+      @merchant_3 = create(:random_merchant)
     end
 
     it 'I can see a list of merchants in the system' do
       visit '/merchants'
 
-      expect(page).to have_link("Brian's Bike Shop")
-      expect(page).to have_link("Meg's Dog Shop")
+      expect(page).to have_link(@merchant_1.name)
+      expect(page).to have_link(@merchant_2.name)
+      expect(page).to have_link(@merchant_3.name)
     end
   end 
 end 
