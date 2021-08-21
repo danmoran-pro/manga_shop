4909127c972dbf26dfb2851e00f437f1e03c2933
Daniel <danmoran.pro@gmail.com>
2021-08-20 20:15:56 -0600

    can update a merchantc

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -21,6 +21,20 @@ class MerchantsController < ApplicationController
     end
   end 
 
+  def edit
+    @merchant = Merchant.find(params[:id])
+  end 
+
+  def update
+    @merchant = Merchant.find(params[:id])
+    @merchant.update(merchant_params)
+    if @merchant.save
+      redirect_to "/merchants/#{@merchant.id}"
+    else
+      flash[:error] = @merchant.errors.full_messages.to_sentence
+      render :edit
+    end
+  end 
   private
   def merchant_params
     params.permit(:name,:address,:city,:state,:zip)
